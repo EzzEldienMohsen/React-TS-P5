fb16f6bf7d129ebfe03852ad377b0a3b9943431f
Ezz dghedy <ezzmohsend@gmail.com>
2024-02-08 15:07:05 +0200

    type safe useSelector hook

--- src/store/cartSlice.ts
+++ src/store/cartSlice.ts
@@ -1,5 +1,4 @@
 import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
-
 type CartItem = {
   id: string;
   title: string;
--- src/store/hooks.ts
+++ src/store/hooks.ts
@@ -1,1 +1,12 @@
-import { UseDispatch } from "react-redux";}
+import {
+  type TypedUseSelectorHook,
+  useDispatch,
+  useSelector,
+} from 'react-redux';
+import { AppDispatch, RootState } from './store';
+
+type DispatchFun = () => AppDispatch;
+
+export const useCartDispatch: DispatchFun = useDispatch;
+
+export const useCartSelector: TypedUseSelectorHook<RootState> = useSelector;

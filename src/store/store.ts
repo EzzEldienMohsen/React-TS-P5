32806a9a7be4213e223e0b053018b3eb0ff52427
Ezz dghedy <ezzmohsend@gmail.com>
2024-02-08 14:13:36 +0200

    adding and removing functionality

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { cartSlice } from './cartSlice';
 
-configureStore({
+export const store = configureStore({
   reducer: {
     cart: cartSlice.reducer,
   },
